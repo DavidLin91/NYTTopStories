c8cd0316d94e3d2276aaa12ee6c901f42534abb5
David <lin.david985@gmail.com>
2020-02-06 10:42:55 -0500

    Initial Commit

---
+++ NYTTopStories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYTTopStories
+//
+//  Created by David Lin on 2/6/20.
+//  Copyright © 2020 David Lin (Passion Proj). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
