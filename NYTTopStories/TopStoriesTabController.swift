4204b29c024cdbd0fde0d57ed666371d78043b4f
David <lin.david985@gmail.com>
2020-02-06 10:49:30 -0500

    edited main screen and deleted storyboard

--- NYTTopStories/TopStoriesTabController.swift
+++ NYTTopStories/TopStoriesTabController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TopStoriesTabController.swift
 //  NYTTopStories
 //
 //  Created by David Lin on 2/6/20.
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class TopStoriesTabController: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .yellow
     }
-
-
+    
+    
+    
 }
-
